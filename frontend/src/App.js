b97ba44715f628332b78ff77969e453e9f7ce01f
jenifergs <contato.comercialjenifer@gmail.com>
2023-02-12 10:37:38 -0300

    feat: Pagina home frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './page/Home';
 
 function App() {
   return (
     <div className="App">
-      ola
+      <Home />
     </div>
   );
 }
--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,9 +1,46 @@
+import ButtonCreate from '../components/ButtonCreate';
+import ButtonDelete from '../components/ButtonDelete';
+import ButtonSearch from '../components/ButtonSearch';
+import Input from '../components/Input';
+import { useState } from 'react';
+import RestrictList from '../components/RestrictList';
+
 function Home () {
+  const [cpf, setCpf] = useState('');
+  const [arrayCustomers, setArrayCustomers] = useState([]);
+
+  const handleChange = (event) => {
+    setCpf(event.target.value);
+  };
+
   return (
+    <main>
+      <h1>Sistema Antifraude</h1>
+      <p>Adicione, remova ou pesquise CPFs </p>
+      <div>
+        <Input
+        value={ cpf }
+        onChange={ handleChange} />
+        <br />
+        <div className='container-btn'>
+        <ButtonCreate cpf={cpf} />
+        <ButtonDelete cpf={cpf} />
+        <ButtonSearch
+         cpf={cpf}
+         setArrayCustomers={setArrayCustomers}
+
+
+         />
+        </div>
+      </div>
     <div>
-      <h1>Home</h1>
+        <h2>Lista de CPFs restritos</h2>
+        <RestrictList
+        arrayCustomers={arrayCustomers}
+         />
     </div>
+    </main>
   )
 }
 
-export default Home
+export default Home;
