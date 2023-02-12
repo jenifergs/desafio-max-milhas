b06ead33b411f924abac71a59733f8fec405bb3f
jenifergs <contato.comercialjenifer@gmail.com>
2023-02-12 10:41:44 -0300

    feat: Componente botao para remover cpf do banco de dados

--- frontend/src/components/ButtonDelete.jsx
+++ frontend/src/components/ButtonDelete.jsx
@@ -1,8 +1,24 @@
-function ButtonDelete(props) {
-  const { onClick, children } = props;
+function ButtonDelete({cpf}) {
+
+  const handleClick = async () => {
+    const options = {method: 'DELETE',
+     headers: {'Content-Type': 'application/json'}};
+
+    const response = await fetch(`http://localhost:3333/cpf/${cpf}`, options)
+
+    if(response.status === 204 ) {
+      alert('Deletado com sucesso')
+    } else {
+      const data = await response.json();      
+      alert(data.message)
+    }
+  
+  };
   return (
-    <button onClick={onClick} className="btn btn-danger">
-      {children}
+    <button
+      onClick={ handleClick }
+      className="btn-remove">
+      Deletar
     </button>
   );
 }
