787ac321387171cc645ba6043554468e0a76b8c4
jenifergs <contato.comercialjenifer@gmail.com>
2023-02-12 10:40:39 -0300

    feat: Componente botão para adicionar cpf no banco de dados

--- frontend/src/components/ButtonCreate.jsx
+++ frontend/src/components/ButtonCreate.jsx
@@ -1,8 +1,26 @@
-function ButtonCreate(props) {
-  const { onClick, children } = props;
+function ButtonCreate({ cpf }) {
+
+  const handleClick = async () => {
+    const options = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({ cpf }),
+    };
+    
+  const response =  await fetch('http://localhost:3333/cpf', options)
+  const data = await response.json();
+  if(response.status === 201){
+    alert('cpf cadastrado no banco de dados com sucesso')
+  } else {
+    alert(data.message)
+  }
+  };
+ 
   return (
-    <button onClick={onClick} className="btn btn-success">
-      {children}
+    <button
+      onClick={ handleClick }
+     className="btn-create">
+      Adicionar
     </button>
   );
 }
