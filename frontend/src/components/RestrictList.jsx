954d67f7f7f230036099a19d48d0de983c495bf5
jenifergs <contato.comercialjenifer@gmail.com>
2023-02-12 11:39:04 -0300

    refactor: Formata data no componente lista

--- frontend/src/components/RestrictList.jsx
+++ frontend/src/components/RestrictList.jsx
@@ -1,11 +1,14 @@
 function RestrictList ({arrayCustomers}) {
+function formatDate (date) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(date).toLocaleDateString('pt-BR', options);
+  }
 
   return (
     <ul>
       {
         arrayCustomers.map((customer, index) => {
-          return <li key={index}>CPF: {customer.cpf}
-           - Criado Em: {customer.createdAt}</li>
+          return <li key={index}>CPF: {customer.cpf} / Criado Em: {formatDate(new Date(customer.createdAt))}</li>
         })
       }
     </ul>
