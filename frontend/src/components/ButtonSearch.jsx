c471f997bf05e3a1be4e0b250391486d895bc342
jenifergs <contato.comercialjenifer@gmail.com>
2023-02-12 10:42:09 -0300

    feat: Componente botão para pesquisar 1 ou todos os cpfs do banco de dados

--- frontend/src/components/ButtonSearch.jsx
+++ frontend/src/components/ButtonSearch.jsx
@@ -1,9 +1,41 @@
-function buttonSearch() {
+function ButtonSearch({cpf, setArrayCustomers}) {
+
+  const handleClick = async () => {
+    console.log(cpf)
+    const url = `http://localhost:3333/cpf/${cpf}`;
+
+    const response = await fetch(url, {
+      method: 'GET',
+    });
+
+    const data = await response.json();
+
+    if(response.status === 200) {
+      if(Array.isArray(data)) {
+        setArrayCustomers(data);
+      } else {
+        setArrayCustomers([data]);
+      }
+
+
+    } else {
+      alert(data.message)
+    }
+
+
+  }
+
+
   return (
-    <div className="buttonSearch">
-      <button type="button">Buscar</button>
+    <div>
+      <button
+       className="btn-search"
+       onClick={ handleClick }
+       type="button">
+          Buscar
+      </button>
     </div>
   );
 }
 
-export default buttonSearch;
+export default ButtonSearch;
